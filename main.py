086a3a98d474a9a5777571c3aa3f25191055c6a8
Kofi <kufuorkofi@gmail.com>
2024-09-10 11:39:21 +0000

    COMMIT

--- main.py
+++ main.py
@@ -679,7 +679,7 @@ def entity():
   ELSE 0
   END AS GRANT_DATE_COUNT
   FROM ARBIGRANTS.DBT.ARBIGRANTS_LABELS_PROJECT_METADATA
-  WHERE NAME = '{grantee_name}'
+  WHERE LOWER(NAME) = LOWER('{grantee_name}')
   ''',
                                 grantee_name=grantee_name)
 
